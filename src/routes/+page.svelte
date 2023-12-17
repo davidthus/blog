8a4e56552822bdaed00b35062496e7d53bd04656
davidthus <dvdnicorici05@gmail.com>
2023-12-17 21:44:33 +0000

    attempt to transition mix blend mode again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
 
 	let isBlendModeDiff: boolean = true;
 
@@ -15,12 +16,15 @@
 </script>
 
 <h1 class="text-8xl w-full max-w-2xl text-center text-text font-roman">
-	The <strong
-		class="text-accent relative inline transition duration-700 mix-blend-difference {isBlendModeDiff
-			? 'opacity-1'
-			: 'opacity-0'} before:content-[Artful_Alcove] before:text-accent before:block before:absolute before:{isBlendModeDiff
-			? 'opacity-0'
-			: 'opacity-1'} before:inset-0 mix-blend-multiply before:transition before:duration-700"
-		>Artful Alcove</strong
-	> of davidthus
+	The {#if isBlendModeDiff}
+		<strong
+			transition:fade={{ delay: 500, duration: 400 }}
+			class="text-accent inline transition duration-700 mix-blend-difference">Artful Alcove</strong
+		>
+	{:else}
+		<strong
+			transition:fade={{ delay: 500, duration: 400 }}
+			class="text-accent inline transition duration-700 mix-blend-multiply">Artful Alcove</strong
+		>
+	{/if} of davidthus
 </h1>
