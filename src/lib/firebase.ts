221da0d254bd8e386bb5dba4a560710f4cea0dd6
davidthus <dvdnicorici05@gmail.com>
2023-12-20 21:53:16 +0000

    create doc store

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,7 @@
+import { doc, getFirestore, onSnapshot } from 'firebase/firestore';
+
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { writable } from 'svelte/store';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyA5LjvFj1cvzji5yCjd4XowOOom4j_C840',
@@ -13,3 +15,23 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
+
+export function docStore<T>(path: string) {
+	let unsubscribe: () => void;
+
+	const docRef = doc(db, path);
+
+	const { subscribe } = writable<T | null>(null, (set) => {
+		unsubscribe = onSnapshot(docRef, (snapshot) => {
+			set((snapshot.data() as T) ?? null);
+		});
+
+		return () => unsubscribe();
+	});
+
+	return {
+		subscribe,
+		ref: docRef,
+		id: docRef.id
+	};
+}
