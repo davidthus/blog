e044a683c714358f8301223f9481b9ad99132e52
davidthus <dvdnicorici05@gmail.com>
2023-12-27 17:14:31 +0000

    remove unecessary file

--- src/routes/(app)/stories/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { collectionStore } from '$lib/firebase';
-import type { PageServerLoad } from './$types';
-
-export const load = (async () => {
-	const collection = collectionStore('stories');
-
-	return { collection };
-}) satisfies PageServerLoad;
--- src/routes/(app)/stories/+page.svelte
+++ src/routes/(app)/stories/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import { collectionStore } from '$lib/firebase';
+	import { query } from 'firebase/firestore';
 
-	export let data: PageData;
+	const collection = collectionStore('stories/gXzFDPQqRuGVH3MlHeg5/chapters');
 </script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<iframe
+	src="https://docs.google.com/document/d/e/2PACX-1vTWQbwTREII2M1hFTp2MRp7fyFsBIT9E5Uw_daMgYwZi9Z82cS_fZo1nyK22PcgEUsZ327HsRlZZFlK/pub?embedded=true"
+></iframe>
