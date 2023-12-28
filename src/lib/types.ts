f60824d71345baaa8acbb7bc4598e79aa0168c4b
davidthus <dvdnicorici05@gmail.com>
2023-12-28 19:08:03 +0000

    make story page

---
+++ src/lib/types.ts
@@ -0,0 +1,11 @@
+export interface Story {
+	name: string;
+	description: string;
+	id: string;
+}
+
+export interface Chapter {
+	name: string;
+	content: string;
+	id: string;
+}
--- src/routes/(app)/stories/+page.svelte
+++ src/routes/(app)/stories/+page.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
 	import { collectionStore } from '$lib/firebase';
-
-	interface Story {
-		name: string;
-		description: string;
-		id: string;
-	}
+	import type { Story } from '$lib/types';
 
 	const collection = collectionStore<Story>('stories');
 </script>
 
-<section class="flex flex-col flex-1 ml-10 gap-4 max-w-xl">
+<section class="flex flex-col flex-1 ml-10 gap-4">
+	<article class="w-fit p-6 rounded-2xl shadow-lg max-w-52 shadow-slate-900 hover:cursor-pointer">
+		<h1 class="font-roman text-text text-2xl">Add story</h1>
+	</article>
 	{#each $collection as story}
-		<article class="w-full p-6 flex flex-col items-start gap-2">
-			<h1 class="font-roman text-white text-2xl">{story?.name}</h1>
-			<p class="font-sharp text-white text-base">{story?.description}</p>
-		</article>
+		<a href="/stories/{story.id}">
+			<article
+				class="w-full p-6 flex flex-col items-start gap-2 rounded-2xl shadow-lg max-w-xl shadow-slate-900 hover:cursor-pointer"
+			>
+				<h1 class="font-roman text-text text-2xl">{story?.name}</h1>
+				<p class="font-sharp text-text text-base">{story?.description}</p>
+			</article>
+		</a>
 	{/each}
 </section>
